27057597645e9b6b1048015f22d27ef21c00044e
Chris <chriscarton@live.fr>
2024-08-11 08:05:13 +0200

    quelques ajustements de qualité

--- src/pages/accueil.php
+++ src/pages/accueil.php
@@ -1,9 +1,8 @@
 <?php 
 require('./sections/accueil/intro.php'); 
-
 require('./elements/decoupe.php'); 
-/*
 require('./sections/accueil/pub.php'); 
+/*
 $texte_banniere = 'NE BOUGEZ PAS';
 require('./elements/banniere.php'); 
 require('./sections/accueil/cafe.php'); 
