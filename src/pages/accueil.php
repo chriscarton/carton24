d0e7baf2cb1027c145380111c2af94612c00183f
Chris <chriscarton@live.fr>
2024-08-16 10:52:22 +0200

    suppression backups intro et amélioration footer

--- src/pages/accueil.php
+++ src/pages/accueil.php
@@ -21,8 +21,12 @@ require('./sections/accueil/cafe.php');
 $texte_banniere = 'installez-vous confortablement';
 require('./elements/banniere.php'); 
 
-require('./sections/accueil/contact.php'); 
-require('./sections/accueil/carte.php'); 
+require('./sections/accueil/contact.php');
+?>
 
+<div id="footerZone">
+<?php
+require('./sections/accueil/carte.php'); 
 require('./elements/footer.php'); 
 ?>
+</div>
